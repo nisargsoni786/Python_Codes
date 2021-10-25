c01ac59f5e565921ba57d3f70204264123d0fbc7
nisarg <nisarg.soni@smartsensesolutions.com>
2021-10-25 15:12:32 +0530

    addition of rabbitmq

---
+++ rabbitmq/consumer2.py
@@ -0,0 +1,19 @@
+import pika
+
+connection = pika.BlockingConnection(
+pika.ConnectionParameters(host='localhost')
+)
+
+channel1 = connection.channel()
+
+def callback(ch,method,properties,body):
+    print("Body>>",body)
+
+channel1.basic_consume(
+    queue='hello2',
+    on_message_callback=callback,
+    auto_ack=True)
+
+
+print("******WAITING****")
+channel1.start_consuming()
--- rabbitmq/producer.py
+++ rabbitmq/producer.py
@@ -7,9 +7,11 @@ pika.ConnectionParameters(host='localhost')
 channel1 = connection.channel()
 
 channel1.queue_declare(queue = "hello")
+channel1.queue_declare(queue = "hello2")
 
 while(1):
     x = input()
     channel1.basic_publish(exchange="", routing_key="hello",body = x)
+    channel1.basic_publish(exchange="", routing_key="hello2",body = x+" 222")
 
 channel1.close()
