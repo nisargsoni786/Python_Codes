a616b6f145dc15bc5f9a336eabe35df179535fe3
Nisarg Soni <63102288+nisargsoni786@users.noreply.github.com>
2022-12-12 17:46:20 +0530

    Update fastapi-decorator.py

--- fastapi-decorator.py
+++ fastapi-decorator.py
@@ -33,6 +33,56 @@ async def get_conversation(
         logger.info("end route")
         response.status_code=300
         return {"path":"/root---222222"}
+            
+    except Exception as e:
+        logger.exception(str(e))
+        return {"ERROR": True}
+
+    
+    
+    
+# =======================================================================================================================================================
+
+
+def mid3(action):
+    def inner(func):
+        @wraps(func)
+        async def __decorator(request, *args, **kwargs):
+            request.state.user = "got requestttttttttt"
+            request_id = str(uuid.uuid4())
+            with logger.contextualize(request_id=request_id):
+                try:
+                    logger.debug("Request started --- {}",action)
+                    return await func(request, *args, **kwargs)
+
+                except Exception as e:
+                    logger.exception(str(e))
+
+                finally:
+                    logger.debug("Request ended")
+
+
+        return __decorator
+    return inner
+
+
+
+
+@root_router.get('/2')
+@mid3("Nisargggg")
+async def get_conversation(
+    request: Request,
+    response: Response,
+    id: str
+):
+    try:
+        logger.info("id is {}",id)
+        logger.info("User {}",request.state.user)
+        logger.info("start route")
+        # root_controller.get_root_controller(request,response)
+        logger.info("end route")
+        response.status_code=300
+        return {"path":"/root---222222"}
         
     except Exception as e:
         logger.exception(str(e))
